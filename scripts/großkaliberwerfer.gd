0174cff0f959a0b74f0981d3f7d9d4088550f4e9
Lara-S-K <114135998+MineKecks@users.noreply.github.com>
2024-05-24 22:47:56 +0200

    granatenwerfer logik

--- scripts/großkaliberwerfer.gd
+++ scripts/großkaliberwerfer.gd
@@ -1,11 +1,12 @@
 extends Node3D
+var speed = 5
+var active = false
 
+@export var shoot_item: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
@@ -13,4 +14,15 @@ func _process(delta):
 
 
 func shoot(): 
-	pass
+	if active:
+		$Cooldown.start()
+		var shoot_item:RigidBody3D = shoot_item.instantiate()
+		var richtung = global_rotation
+		shoot_item.apply_central_force(richtung*speed)
+		
+		
+	
+
+
+func _on_Cooldown_timeout():
+	active = true
