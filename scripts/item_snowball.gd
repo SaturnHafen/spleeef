b54aa616de46dce1f0782e1c07a644906792c786
SaturnHafen <37272151+SaturnHafen@users.noreply.github.com>
2024-05-25 12:16:39 +0200

    add snowball

---
+++ scripts/item_snowball.gd
@@ -0,0 +1,29 @@
+extends RigidBody3D
+
+var projectile_root: Node3D
+var just_shot = false
+var direction: Vector3
+
+@export var force_multiplier: float = 40
+
+func shoot():
+	just_shot = true
+	freeze = false
+	
+	var shoot_position = global_position
+	direction = global_transform.basis.x
+	
+	get_parent().remove_child(self)
+	projectile_root.add_child(self)
+	global_position = shoot_position
+	$Fuse.start()
+
+func _integrate_forces(state):
+	if just_shot:
+		just_shot = false
+		
+		apply_central_impulse((direction + Vector3.UP * 0.1) * force_multiplier)
+
+
+func _on_fuse_timeout():
+	queue_free()
