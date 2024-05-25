b71e8a7f7bbd739469382e4f413715e815b51833
Marius Dörbandt <github.marius@doerbandt.de>
2024-05-25 20:09:56 +0200

    Add laser knockback

--- scripts/projectiles/laser.gd
+++ scripts/projectiles/laser.gd
@@ -3,11 +3,24 @@ extends Area3D
 const graph = preload("res://resources/sdfs/laser.tres")
 const length = 20.0
 const radius = 0.5
+const knockback = 5
+
+func _ready():
+	# fix random rotation toggling at spawn
+	$Mesh.rotation_degrees.z = 90
 
 func _do_destruction():
+	var direction = global_transform.basis.x.normalized()
 	Voxels.do_graph(
 		graph,
-		global_position + global_transform.basis.x.normalized() * length / 2.0,
+		global_position + direction * length / 2.0,
 		Vector3(length * 5, radius * 10, radius * 10),
 		global_rotation.z,
 		global_rotation.y)
+
+	var collisions = get_overlapping_bodies()
+	for body in collisions:
+		if body.is_in_group("player"):
+			var delta = body.global_position - global_position
+			delta -= direction * delta.dot(direction)
+			body.knockback += delta.normalized() / delta.length() * knockback
