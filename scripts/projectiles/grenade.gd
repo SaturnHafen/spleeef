e5d6c77dd0517ba644197df8d514202e261ceaed
Marius Dörbandt <github.marius@doerbandt.de>
2024-05-25 20:19:21 +0200

    Add explosion to grenade

--- scripts/projectiles/grenade.gd
+++ scripts/projectiles/grenade.gd
@@ -6,10 +6,14 @@ var graph = preload("res://resources/sdfs/noisy_sphere.tres");
 
 func on_timer_timeout():
 	Voxels.do_graph(graph, global_position, Vector3(35, 35, 35))
-	queue_free()
+	var explosion = preload("res://scenes/explosion.tscn").instantiate()
+	get_parent().add_child(explosion)
+	explosion.global_position = global_position
 	
 	var collisions = $KnockbackArea.get_overlapping_bodies()
 	for body in collisions:
 		if body.is_in_group("player"):
 			var delta = body.position - position
 			body.knockback += delta.normalized() / delta.length() * knockback
+	
+	queue_free()
