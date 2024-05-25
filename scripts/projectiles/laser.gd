9a61dbea3ecd08dee3281627cc7e7c5f92439394
Marius Dörbandt <github.marius@doerbandt.de>
2024-05-25 19:40:47 +0200

    Implement laser destruction

--- scripts/projectiles/laser.gd
+++ scripts/projectiles/laser.gd
@@ -1,8 +1,13 @@
 extends Area3D
 
-@export var graph: VoxelGeneratorGraph
+const graph = preload("res://resources/sdfs/laser.tres")
+const length = 20.0
+const radius = 0.5
 
 func _do_destruction():
-	pass
-	#var terrain = %VoxelLodTerrain
-	#terrain.destroy_stuff(graph)
+	Voxels.do_graph(
+		graph,
+		global_position + global_transform.basis.x.normalized() * length / 2.0,
+		Vector3(length * 5, radius * 10, radius * 10),
+		global_rotation.z,
+		global_rotation.y)
--- scripts/voxels.gd
+++ scripts/voxels.gd
@@ -3,8 +3,11 @@ extends Node
 var terrain: VoxelLodTerrain
 var voxel_tool: VoxelToolLodTerrain
 
-func do_graph(graph: VoxelGeneratorGraph, global_pos: Vector3, size: Vector3):
+func do_graph(graph: VoxelGeneratorGraph, global_pos: Vector3, size: Vector3, pitch = 0.0, yaw = 0.0):
 	var to_local = terrain.global_transform.affine_inverse()
 	var pos = to_local * global_pos
-	var transform = Transform3D.IDENTITY.translated(pos)
+	var transform = Transform3D.IDENTITY \
+		.rotated(Vector3(0, 0, 1), pitch) \
+		.rotated(Vector3(0, 1, 0), yaw) \
+		.translated(pos)
 	voxel_tool.do_graph(graph, transform, size)
