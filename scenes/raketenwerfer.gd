31caaf614d98b723eed50c5e9fcfcf35389952fd
Lara-S-K <114135998+MineKecks@users.noreply.github.com>
2024-05-25 17:30:17 +0200

    raketenwerfer eigenen schuss fkt

--- scenes/raketenwerfer.gd
+++ scenes/raketenwerfer.gd
@@ -24,7 +24,7 @@ func shoot():
 		$Cooldown.start()
 		var item: RigidBody3D = shoot_item.instantiate()
 		var direction = global_transform.basis.x
-		item.apply_central_force(direction * speed)
+		item.add_constant_central_force(direction * speed)
 		projectile_root.add_child(item)
 		item.global_position = global_position + direction * offset
 		active = false
