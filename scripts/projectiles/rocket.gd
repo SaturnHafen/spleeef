248a168b6fe2c2343e90d71ec44d806edadb159e
SaturnHafen <37272151+SaturnHafen@users.noreply.github.com>
2024-05-25 20:09:15 +0200

    Add explosion to rocket

--- scripts/projectiles/rocket.gd
+++ scripts/projectiles/rocket.gd
@@ -5,6 +5,7 @@ extends RigidBody3D
 func explode():
 	var exp = explosion.instantiate()
 	get_parent().add_child(exp)
+	exp.global_position = global_position
 	queue_free()
 
 func _on_body_entered(body):
